9e3cdb23470a33861d6787b25584e41973916cf9
caveeroo <62477582+caveeroo@users.noreply.github.com>
2023-03-23 18:10:32 +0100

    Update WebSecurityConfig.java

--- src/main/java/org/owasp/webgoat/container/WebSecurityConfig.java
+++ src/main/java/org/owasp/webgoat/container/WebSecurityConfig.java
@@ -43,7 +43,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.StandardPasswordEncoder
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
 
 /** Security configuration for WebGoat. */
 @Configuration
