4a8a49a6806a287fce122ece7f99817c3ec8f214
Diego-AltF4 <diegooaltf4@gmail.com>
2023-04-14 18:15:44 +0200

    sqli fixed

--- src/main/java/org/owasp/webgoat/lessons/sqlinjection/introduction/SqlInjectionLesson10.java
+++ src/main/java/org/owasp/webgoat/lessons/sqlinjection/introduction/SqlInjectionLesson10.java
@@ -22,10 +22,8 @@
 
 package org.owasp.webgoat.lessons.sqlinjection.introduction;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+
 import org.owasp.webgoat.container.LessonDataSource;
 import org.owasp.webgoat.container.assignments.AssignmentEndpoint;
 import org.owasp.webgoat.container.assignments.AssignmentHints;
@@ -60,15 +58,16 @@ public class SqlInjectionLesson10 extends AssignmentEndpoint {
   }
 
   protected AttackResult injectableQueryAvailability(String action) {
-    StringBuilder output = new StringBuilder();
-    String query = "SELECT * FROM access_log WHERE action LIKE '%" + action + "%'";
+      StringBuilder output = new StringBuilder();
+      String query = "SELECT * FROM access_log WHERE action LIKE ?";
 
-    try (Connection connection = dataSource.getConnection()) {
-      try {
-        Statement statement =
-            connection.createStatement(
-                ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-        ResultSet results = statement.executeQuery(query);
+      //String query = "SELECT * FROM access_log WHERE action LIKE '%" + action + "%'";
+
+      try (Connection connection = dataSource.getConnection()) {
+          try {
+              PreparedStatement statement = connection.prepareStatement(query, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+              statement.setString(1, "%" + action + "%");
+              ResultSet results = statement.executeQuery();
 
         if (results.getStatement() != null) {
           results.first();
