8ed3fc77525a720d5359b38a218d9190947dd5f0
monica ramos <m.ramosm.2021@alumnos.urjc.es>
2024-04-04 18:22:21 +0200

    JWT4

--- src/main/java/org/owasp/webgoat/lessons/jwt/JWTRefreshEndpoint.java
+++ src/main/java/org/owasp/webgoat/lessons/jwt/JWTRefreshEndpoint.java
@@ -134,7 +134,7 @@ public class JWTRefreshEndpoint extends AssignmentEndpoint {
     String refreshToken;
     try {
       Jwt<Header, Claims> jwt =
-          Jwts.parser().setSigningKey(JWT_PASSWORD).parsetoken.replace("Bearer ", ""));
+          Jwts.parser().setSigningKey(JWT_PASSWORD).parse(token.replace("Bearer ", ""));
       user = (String) jwt.getBody().get("user");
       refreshToken = (String) json.get("refresh_token");
     } catch (ExpiredJwtException e) {
