54a1211d43e3fe18797802f2076e768a33d400ea
dtejada02 <100412699+dtejada02@users.noreply.github.com>
2024-04-05 11:30:27 +0200

    Ejercicio1.py

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,14 +1,10 @@
 import re
 
-# Texto de ejemplo
 text = "Este es un texto con algunos años como 2022 y 2024, pero también tiene 2023, 1000, 0020, 30, 2020, 0000"
 
-# Expresión regular para encontrar años de 4 dígitos
 regex = r"\b\d{4}\b"
 
-# Buscar todos los años en el texto
 years = re.findall(regex, text)
 
-# Imprimir los años encontrados en orden de aparición
 for year in years:
     print(year)
