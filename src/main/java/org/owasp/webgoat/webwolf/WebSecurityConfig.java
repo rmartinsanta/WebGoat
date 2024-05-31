efd464b8cf79f872e2352454ced12247d4cb65a1
iN3ast <l.martinezb.2021@alumnos.urjc.es>
2024-05-31 21:10:50 +0200

    Update WebSecurityConfig.java to use BCryptPasswordEncoder for password encoding 2.0

--- src/main/java/org/owasp/webgoat/webwolf/WebSecurityConfig.java
+++ src/main/java/org/owasp/webgoat/webwolf/WebSecurityConfig.java
@@ -35,6 +35,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.BCryptPasswordEncoder;
 
 /** Security configuration for WebGoat. */
 @Configuration
@@ -64,7 +66,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Autowired
   public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-    auth.userDetailsService(userDetailsService); // .passwordEncoder(bCryptPasswordEncoder());
+    auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
+    
   }
 
   @Bean
