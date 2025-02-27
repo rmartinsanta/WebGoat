a063c319b9b2b0d859cd8df76fc317ef53a0b913
dtorresss <diegotorresprofesional@gmail.com>
2025-02-27 19:26:16 +0100

    Ahora bcrypt debe funcionar bien

--- src/main/java/org/owasp/webgoat/container/WebSecurityConfig.java
+++ src/main/java/org/owasp/webgoat/container/WebSecurityConfig.java
@@ -42,7 +42,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.BCryptPasswordEncoder;
 
 /** Security configuration for WebGoat. */
 @Configuration
