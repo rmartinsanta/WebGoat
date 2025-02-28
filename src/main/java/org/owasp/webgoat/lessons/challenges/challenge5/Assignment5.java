bb60049303cbe939d80e7697b74254b4063d5030
Xerneax8 <148555019+Xerneax8@users.noreply.github.com>
2025-02-28 12:42:34 +0100

    Fix Format Assignment5.java

--- src/main/java/org/owasp/webgoat/lessons/challenges/challenge5/Assignment5.java
+++ src/main/java/org/owasp/webgoat/lessons/challenges/challenge5/Assignment5.java
@@ -41,32 +41,33 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class Assignment5 extends AssignmentEndpoint {
 
-  private final LessonDataSource dataSource;
-  private final Flags flags;
+    private final LessonDataSource dataSource;
+    private final Flags flags;
 
-  @PostMapping("/challenge/5")
-  @ResponseBody
-  public AttackResult login(
-      @RequestParam String username_login, @RequestParam String password_login) throws Exception {
-    if (!StringUtils.hasText(username_login) || !StringUtils.hasText(password_login)) {
-      return failed(this).feedback("required4").build();
-    }
-    if (!"Larry".equals(username_login)) {
-      return failed(this).feedback("user.not.larry").feedbackArgs(username_login).build();
-    }
-    try (var connection = dataSource.getConnection()) {
-        String query = "SELECT * FROM users WHERE user = ? AND pass = ?";
-
-        try (PreparedStatement statement = connection.prepareStatement(query)) {
-          statement.setString(1, username_login);
-          statement.setString(2, password_login);
-          ResultSet resultSet = statement.executeQuery();
-    
-          if (resultSet.next()) {
-            return success(this).feedback("challenge.solved").feedbackArgs(flags.getFlag(5)).build();
-          } else {
-            return failed(this).feedback("challenge.close").build();
-          }
+    @PostMapping("/challenge/5")
+    @ResponseBody
+    public AttackResult login(
+            @RequestParam String username_login, @RequestParam String password_login) throws Exception {
+        if (!StringUtils.hasText(username_login) || !StringUtils.hasText(password_login)) {
+            return failed(this).feedback("required4").build();
         }
-   }
+        if (!"Larry".equals(username_login)) {
+            return failed(this).feedback("user.not.larry").feedbackArgs(username_login).build();
+        }
+        try (var connection = dataSource.getConnection()) {
+            String query = "SELECT * FROM users WHERE user = ? AND pass = ?";
+
+            try (PreparedStatement statement = connection.prepareStatement(query)) {
+                statement.setString(1, username_login);
+                statement.setString(2, password_login);
+                ResultSet resultSet = statement.executeQuery();
+
+                if (resultSet.next()) {
+                    return success(this).feedback("challenge.solved").feedbackArgs(flags.getFlag(5)).build();
+                } else {
+                    return failed(this).feedback("challenge.close").build();
+                }
+            }
+        }
+    }
 }
