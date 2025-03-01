031bcaa381b5e317071f47d3743439d05625d031
vbv-18 <shafariens23@gmail.com>
2025-03-01 20:39:58 +0100

    Fix SqlInjectionChallenge with PreparedStatements

--- src/main/java/org/owasp/webgoat/lessons/sqlinjection/advanced/SqlInjectionChallenge.java
+++ src/main/java/org/owasp/webgoat/lessons/sqlinjection/advanced/SqlInjectionChallenge.java
@@ -54,7 +54,7 @@ public class SqlInjectionChallenge extends AssignmentEndpoint {
   // assignment path is bounded to class so we use different http method :-)
   @ResponseBody
   public AttackResult registerNewUser(
-      @RequestParam String username_reg,
+      @RequestParam String username_reg, //1 y 2
       @RequestParam String email_reg,
       @RequestParam String password_reg)
       throws Exception {
@@ -63,10 +63,15 @@ public class SqlInjectionChallenge extends AssignmentEndpoint {
     if (attackResult == null) {
 
       try (Connection connection = dataSource.getConnection()) {
-        String checkUserQuery =
-            "select userid from sql_challenge_users where userid = '" + username_reg + "'";
-        Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery(checkUserQuery);
+       String checkUserQuery =
+            "select userid from sql_challenge_users where userid = '" + username_reg + "'"; //3,4,5
+
+        //new secure query 
+        PreparedStatement preparedStatement1 = connection.prepareStatement(checkUserQuery);
+        preparedStatement1.setString(1, username_reg);
+
+        //Statement statement = connection.createStatement();
+        ResultSet resultSet = preparedStatement1.executeQuery(); //6
 
         if (resultSet.next()) {
           if (username_reg.contains("tom'")) {
