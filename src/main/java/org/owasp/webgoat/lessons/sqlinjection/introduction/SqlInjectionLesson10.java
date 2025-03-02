c970230a426de9f02836be423733792e03fca8c3
panecillouis <paula54carrascosa85@gmail.com>
2025-03-02 16:54:49 +0100

    fix vulnerability SQL injection in SqlInjectionLesson10.java

--- src/main/java/org/owasp/webgoat/lessons/sqlinjection/introduction/SqlInjectionLesson10.java
+++ src/main/java/org/owasp/webgoat/lessons/sqlinjection/introduction/SqlInjectionLesson10.java
@@ -61,14 +61,14 @@ public class SqlInjectionLesson10 extends AssignmentEndpoint {
 
   protected AttackResult injectableQueryAvailability(String action) {
     StringBuilder output = new StringBuilder();
-    String query = "SELECT * FROM access_log WHERE action LIKE '%" + action + "%'";
+    String query = "SELECT * FROM access_log WHERE action LIKE ?";
 
     try (Connection connection = dataSource.getConnection()) {
-      try {
-        Statement statement =
-            connection.createStatement(
-                ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-        ResultSet results = statement.executeQuery(query);
+        // Usamos PreparedStatement en lugar de Statement
+        try (PreparedStatement statement = connection.prepareStatement(query, 
+                ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY)) {
+            statement.setString(1, "%" + action + "%");
+            ResultSet results = statement.executeQuery()
 
         if (results.getStatement() != null) {
           results.first();
