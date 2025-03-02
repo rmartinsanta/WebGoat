2e40d658765237deecbda513f8570c951d78fd5b
panecillouis <paula54carrascosa85@gmail.com>
2025-03-02 18:00:39 +0100

    Fix SQL Injection vulnerability in Servers.java

--- src/main/java/org/owasp/webgoat/lessons/sqlinjection/mitigation/Servers.java
+++ src/main/java/org/owasp/webgoat/lessons/sqlinjection/mitigation/Servers.java
@@ -26,6 +26,7 @@ import java.util.ArrayList;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import java.sql.PreparedStatement;
 import lombok.extern.slf4j.Slf4j;
 import org.owasp.webgoat.container.LessonDataSource;
 import org.springframework.http.MediaType;
@@ -67,27 +68,25 @@ public class Servers {
   public List<Server> sort(@RequestParam String column) throws Exception {
     List<Server> servers = new ArrayList<>();
 
+    String query = "select id, hostname, ip, mac, status, description from SERVERS where status <> 'out of order' order by ?";
     try (var connection = dataSource.getConnection()) {
-      try (var statement =
-          connection.prepareStatement(
-              "select id, hostname, ip, mac, status, description from SERVERS where status <> 'out"
-                  + " of order' order by "
-                  + column)) {
-        try (var rs = statement.executeQuery()) {
-          while (rs.next()) {
-            Server server =
-                new Server(
-                    rs.getString(1),
-                    rs.getString(2),
-                    rs.getString(3),
-                    rs.getString(4),
-                    rs.getString(5),
-                    rs.getString(6));
-            servers.add(server);
-          }
+      try (var statement = connection.prepareStatement(query)) {
+      statement.setString(1, column);
+      try (var rs = statement.executeQuery()) {
+        while (rs.next()) {
+        Server server =
+          new Server(
+            rs.getString(1),
+            rs.getString(2),
+            rs.getString(3),
+            rs.getString(4),
+            rs.getString(5),
+            rs.getString(6));
+        servers.add(server);
         }
+      }
       }
     }
     return servers;
+    }
   }
-}
